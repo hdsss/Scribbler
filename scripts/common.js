2ea992ec7018dcfbbd1eadb3d5c0972328033fb7
hsisodi <hridesh.sisodia@daimler.com>
2020-12-14 12:24:32 +0530

    sign in Modal created

--- scripts/common.js
+++ scripts/common.js
@@ -1,24 +1,39 @@
-var modal = document.getElementById("myModal");
+var signUpModal = document.getElementById("signUpModal");
+var signInModal = document.getElementById("signInModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("signUpBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the button that opens the signUpModal
+var signUpBtn = document.getElementById("signUpBtn");
+var signInBtn = document.getElementById("signInBtn");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+// Get the <span> element that closes the signUpModal
+var closeSignUp = document.getElementsByClassName("close")[0];
+var closeSignIn = document.getElementsByClassName("close")[1];
+
+// When the user clicks on the button, open the signUpModal
+signUpBtn.onclick = function() {
+  signUpModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+signInBtn.onclick = function() {
+    signInModal.style.display = "block";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks on <span> (x), close the signUpModal
+closeSignUp.onclick = function() {
+  signUpModal.style.display = "none";
+}
+
+closeSignIn.onclick = function() {    
+    signInModal.style.display = "none";
+  }
+
+// When the user clicks anywhere outside of the signUpModal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == signUpModal) {
+    signUpModal.style.display = "none";
+  }
+  if (event.target == signInModal) {
+    signInModal.style.display = "none";
   }
 }
